fbf2804a5c3cfedcbad99aec5dea7840ebd83456
monteiro244 <jv8515409@gmail.com>
2024-09-18 13:54:25 +0000

    criaçao de prompt

---
+++ aula.js
@@ -0,0 +1,12 @@
+var prompt = require('prompt-sync')();
+
+let atendimento = prompt("oi, tudo bem? O que vc deseja?")
+
+console.log('oi, tudo sim! Estou procurando uma peça de roupa para a minha filha.')
+
+let atendimento = prompt("hmmm, que gal! E vc ja sabe qual tipo de peça vai querer?")
+
+console.log('sim,sim! Vou escolher uma blusa pra ela.')
+
+let atendimento = prompt("temos essas daqui, pode dar uma olhadinha e ver qual vc acha que combinaria mais coma a sua filha")
+
