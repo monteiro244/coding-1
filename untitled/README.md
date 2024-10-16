8cfdc803934291b47f1c857d400d278ff0cac489
monteiro244 <jv8515409@gmail.com>
2024-10-16 10:48:05 -0300

    Add files via upload

---
+++ untitled/README.md
@@ -0,0 +1,4 @@
+# Untitled
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/monteiro244/pen/ZEgKyeQ](https://codepen.io/monteiro244/pen/ZEgKyeQ).
+
---
+++ untitled/src/script.js
@@ -0,0 +1,7 @@
+let livros = ["javascript", "ecamascipt", "mongodb"];
+//remove o ultimo elemento e mostra o elemento removido.
+console.log(livros.pop());
+
+//adiciona elementos no final da array e mostra a quantidade do array
+console.log(livros.push("novolivro"));
+console.log(livros)
